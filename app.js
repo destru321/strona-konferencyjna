0eb66d660372bb96e82a634bfca331ac48a1ca8e
Dominik Machowski <dominikmachowski321@icloud.com>
2023-03-07 15:15:47 +0100

    serving files

---
+++ app.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+app.use(express.static('public'));
+app.use(express.json());
+
+
+app.get('/', (req,res) => {
+    res.send();
+})
+
+const endpoints = [
+    '/',
+    '/about',
+    '/invitedSpeakers',
+    '/programme',
+    '/deadlinesFees',
+    '/commitees',
+    '/registration',
+    '/venueTravel'
+]
+
+endpoints.forEach(endpoint => {
+    app.post(endpoint, (req,res) => {
+        if(endpoint != '/') {
+            let file = req.body.fileName;
+            res.sendFile(path.join(__dirname, `views/${file}`));
+        } else {
+            res.sendFile(path.join(__dirname, `views/home.html`));
+        }
+    })
+})
+
+app.listen(3000);
