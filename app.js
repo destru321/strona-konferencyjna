f6da3fe4b7dcf9a4b29af4fc10000b16ddae14db
Dominik Machowski <dominikmachowski321@icloud.com>
2023-03-08 10:41:11 +0100

    commmit

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const path = require('path');
+const cors = require('cors');
 
 const app = express();
 
 app.use(express.static('public'));
 app.use(express.json());
+app.use(cors());
 
 
 app.get('/', (req,res) => {
